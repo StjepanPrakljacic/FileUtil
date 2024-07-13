80de367ab1e62c4a9ab319c3447535fcefaa89d4
StjepanPrakljacic <stjepanprakljacic@gmail.com>
2024-07-13 18:47:38 +0200

    feture/FileUtil-init: Inital commit of modules.

---
+++ FileUtil/Utils.py
@@ -0,0 +1,15 @@
+###############################################################################
+# FileUtil - Utility Functions
+#
+# Description: This module provides utility functions that support file and
+#              directory operations, such as handling file paths and checking
+#              file extensions.
+#
+# Created by: Stjepan Prakljačić
+# License: MIT License
+# Repository: https://github.com/StjepanPrakljacic/FileUtil
+#
+# Version: 1.0.0
+###############################################################################
+
+
---
+++ FileUtil/file_operations.py
@@ -0,0 +1,13 @@
+###############################################################################
+# FileUtil - File Operations
+#
+# Description: This module provides functions for performing file operations
+#              such as reading from, writing to, and appending to files.
+#
+# Created by: Stjepan Prakljačić
+# License: MIT License
+# Repository: https://github.com/StjepanPrakljacic/FileUtil
+#
+# Version: 1.0.0
+###############################################################################
+
